e253d607a60743cc06cd1de0bc416881782f6d67
Anudle <AnuShrestha007@gmail.com>
2016-09-25 16:11:57 -0600

    added create and delete event

--- database/query.js
+++ database/query.js
@@ -21,7 +21,34 @@ module.exports = {
   getAllEvents: Events,
   getAllActivites: Activities,
   getAllUsers: Users,
+
   getActivitesByName: function(name){
-    return Activities().where('activity_name', name);
+    return
+    Activities().where('activity_name', name);
   },
+
+  addEvent: function(title, activity_id, description, location, when){
+   return Events().insert({
+     title: title,
+     activity_id: activity_id,
+     description: description,
+     location, when
+   })
+ },
+
+  getEventByUsers: function(user){
+    return Events().where('users_id', user);
+ }
+
+  getEventByLocation: function(location){
+    return Events().where('location', location);
+}
+
+  getEventByTime: function(time){
+    return Events().where('when', time);
+}
+
+ deleteEvent: function(id){
+   return Events().where('id', id).del();
+ },
 }
--- migrations/20160923140823_event.js
+++ migrations/20160923140823_event.js
@@ -2,6 +2,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('event', function(table) {
         table.increments();
+        table.string('title')
         table.integer('activity_id').references('id').inTable('activity').onDelete('CASCADE');
         table.integer('user_id').references('id').inTable('users').onDelete('CASCADE');
         table.string('description');
