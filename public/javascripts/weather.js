e13fd1cf8fc78d4cb09deb6c2d33feb2e546309e
catiecook <catiecook888@gmail.com>
2016-09-26 15:54:11 -0600

    worked on api

--- public/javascripts/weather.js
+++ public/javascripts/weather.js
@@ -3,14 +3,14 @@ $('dashboard.hbs').ready(function(e) {
 
   let $urlBase = 'http://api.wunderground.com/api/';
   let weatherAPI = weatherAPI;
-  let $zip /*pull in zipcode from user info with query */
+  let $query /* pull in zipcode or city,state from user info with query */
 
-  let $url = $urlBase + weatherAPI + '/q/' + $zip + '&format==json';
+  let $url = $urlBase + weatherAPI + '/adfd7dc0d7a5f2f7/forecast/q/80211.json';
 
   $get($url).then(function(data){
     console.log(data);
     //set up results
-    let $results = $('<p />', {
+    let $results = $('<div />', {
       "class": 'card-text'
     })
     //append the api call results into given div
