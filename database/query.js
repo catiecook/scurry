60ad51fd5e271383b53b86affc65a73f8bf0664b
Anudle <AnuShrestha007@gmail.com>
2016-09-26 09:59:03 -0600

    fixed user bugs

--- database/query.js
+++ database/query.js
@@ -1,54 +1,55 @@
 var knex = require('./knex');
 
 
-function Events(){
-  return knex('event');
+function Events() {
+    return knex('event');
 }
 
-function Activities(){
-  return knex('activity');
+function Activities() {
+    return knex('activity');
 }
 
-function Users(){
-  reutrn knex('users');
-}
+// function Users() {
+//     reutrn knex('users');
+// }
 
 
 
 
 
 module.exports = {
-  getAllEvents: Events,
-  getAllActivites: Activities,
-  getAllUsers: Users,
+    getAllEvents: Events,
+    getAllActivites: Activities,
+    // getAllUsers: Users,
 
-  getActivitesByName: function(name){
-    return
-    Activities().where('activity_name', name);
-  },
+    getActivitesByName: function(name) {
+        return
+        Activities().where('activity_name', name);
+    },
 
-  addEvent: function(title, activity_id, description, location, when){
-   return Events().insert({
-     title: title,
-     activity_id: activity_id,
-     description: description,
-     location, when
-   })
- },
+    addEvent: function(title, activity_id, description, location, when) {
+        return Events().insert({
+            title: title,
+            activity_id: activity_id,
+            description: description,
+            location,
+            when
+        })
+    },
 
-  getEventByUsers: function(user){
-    return Events().where('users_id', user);
- }
+    // getEventByUsers: function(user) {
+    //     return Events().where('users_id', user);
+    // },
 
-  getEventByLocation: function(location){
-    return Events().where('location', location);
-}
+    getEventByLocation: function(location) {
+        return Events().where('location', location);
+    },
 
-  getEventByTime: function(time){
-    return Events().where('when', time);
-}
+    getEventByTime: function(time) {
+        return Events().where('when', time);
+    },
 
- deleteEvent: function(id){
-   return Events().where('id', id).del();
- },
+    deleteEvent: function(id) {
+        return Events().where('id', id).del();
+    },
 }
