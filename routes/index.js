a3cd712d77ee6a5236fd413ef8dba3e5478ce72b
catiecook <catiecook888@gmail.com>
2016-09-26 09:59:05 -0600

    got most of the pages set up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,79 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+//*********************
+// ******* GETS **********
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Scurry' });
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {
+    title: 'Scurry' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', {
+    title: 'Scurry' });
+});
+
+router.get('/create-profile', function(req, res, next) {
+  res.render('create-profile', {
+    title: 'Scurry' });
+});
+
+router.get('/find-activity', function(req, res, next) {
+  res.render('find-activity', {
+    title: 'Scurry' });
+});
+
+router.get('/create-activity', function(req, res, next) {
+  res.render('create-activity', {
+    title: 'Scurry' });
+});
+
+router.get('/scurry-activity', function(req, res, next) {
+  res.render('scurry-activity', {
+    title: 'Scurry' });
+});
+
+router.get('/scurry-activity/:id', function(req, res, next) {
+  res.render('activity', {
+    title: 'Scurry' });
+});
+
+//*********************
+// ***** POSTS ********
+
+router.post('/login', function(req, res, next){
+  res.redirect('/dashboard')
+})
+
+router.post('/resgister', function(req, res, next){
+  res.redirect('/create-profile')
+})
+
+router.post('/create-profile', function(req, res, next){
+  res.redirect('/dashboard')
+})
+
+router.post('/create-activity', function(req, res, next){
+  res.redirect('/dashboard')
+})
+
+router.post('/create-activity', function(req, res, next){
+  res.redirect('/dashboard')
+})
+
+router.post('/scurry-activity', function(req, res, next){
+  res.redirect('/scurry-activity')
+})
+
+router.post('/scurry-activity/:id', function(req, res, next){
+  res.redirect('/activity')
+})
+
 
 module.exports = router;
