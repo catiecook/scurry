01b9fe06dcf5446e420be68d81bffcda005c3f49
Anudle <AnuShrestha007@gmail.com>
2016-09-27 14:48:40 -0600

    create activity

--- migrations/20160923134909_users.js
+++ migrations/20160923134909_users.js
@@ -4,8 +4,6 @@ exports.up = function(knex, Promise) {
         table.string('facebook_id');
         table.string('picture');
         table.string('name');
-        // table.string('token');
-        // table.integer('event_id').references('id').inTable('event')
     })
 };
 
