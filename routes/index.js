3a6c638858ad8128b23fa0c6e9d8047080a35844
catiecook <catiecook888@gmail.com>
2016-09-27 10:25:47 -0600

    working on weather api

--- routes/index.js
+++ routes/index.js
@@ -28,10 +28,10 @@ router.get('/create-profile', function(req, res, next) {
 router.get('/dashboard', function(req, res, next) {
   res.render('dashboard', {
     title: 'Scurry'
-    // allWeather: data,
-    // weatherIcon: weatherIcon,
-    // date: date,
-    // forecast: forecast
+    allWeather: data,
+    weatherIcon: weatherIcon,
+    date: date,
+    forecast: forecast
   })
 });
 
