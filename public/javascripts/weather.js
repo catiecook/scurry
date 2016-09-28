8780513eb0998288f86d646f3097efd479ae4756
catiecook <catiecook888@gmail.com>
2016-09-28 11:49:37 -0600

    getting weather api set up still

--- public/javascripts/weather.js
+++ public/javascripts/weather.js
@@ -1,31 +1,28 @@
-// var express = require('express')
-// var request = require('request')
-// var query = require('../database/query.js')
-//
-// let $query = query.getUsersLocationByID() /* pull in zipcode or city,state from user info with query */
-//
-//
-//   var weatherIcon = []
-//   var forecast = []
-//   var date = []
-//
-//   request('http://api.wunderground.com/api/forecast/q/' + $query + '.json', function(err, res, data) {
-//     if(!err && response.statusCode == 200) {
-//       console.log(data.response.forecast)
-//       var weatherData = data.response.forecast
-//
-//       for(var i=0; i<4; i++){
-//         weatherIcon.push(weatherData.forecastday[i].icon_url)
-//         forecast.push({weatherData.title: weatherData.fcttext_metric})
-//         date.push(weatherData.date)
-//       }
-//
-//       res.render('/dashboard') {
-//
-//       })
-//
-//     }
-//   })
-//
-//
-// module.exports = router;
+var express = require('express')
+var request = require('request')
+var query = require('../../database/query.js')
+
+let api = process.env.weatherAPI
+let city
+let state
+
+
+  // var weatherIcon = []
+  // var forecast = []
+  // var date = []
+
+  request('http://api.wunderground.com/'+api+'/geolocate/q/' + state + city + '.json', function(err, response, data) {
+
+    if(!err && response.statusCode == 200) {
+      console.log(data.response.forecast)
+
+      var lat = data.response.location.lat
+      var long = data.response.location.long
+
+      res.redirect('/dashboard')
+    }
+  })
+
+
+
+module.exports = request;
